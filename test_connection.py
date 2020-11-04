0e420823ad39502457f6a08885d80692dc39e28d
Sergei Vakunov <bakunobu@gmail.com>
2020-11-04 22:34:48 +0300

    connection tested

--- test_connection.py
+++ test_connection.py
@@ -3,7 +3,17 @@ import psycopg2
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
-conn_str = os.environ.get(URL)
+conn_str = os.environ.get('URL')
 conn = psycopg2.connect(conn_str)
 
 
+cursor = conn.cursor()
+
+cursor.execute('CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);')
+
+cursor.execute('INSERT INTO test (num, data) VALUES (%s, %s)',
+               (100, "abc'def"))
+
+cursor.execute('SELECT * FROM test;')
+
+print(cursor.fetchone())
